1e39e9865690bf928ff14116391451ff6188ae70
Ronel Carlo Berino <ronel.carlo@pinnaclecart.com>
2023-05-05 18:09:40 +0800

    Updated add/edit template

--- views/products/add.php
+++ views/products/add.php
@@ -1,43 +1,29 @@
 <div class="w-2/5 my-4">
 	<h1 class="text-2xl mb-10">Add Product</h1>
 	
-	<?php include_once ROOT_DIR . '/views/includes/alert-error.php'; ?>
-	<?php include_once ROOT_DIR . '/views/includes/alert-success.php'; ?>
-	
 	<form method="POST">
+		<?php include_once ROOT_DIR . '/views/includes/alert-error.php'; ?>
+		<?php include_once ROOT_DIR . '/views/includes/alert-success.php'; ?>
+
 		<div>
 			<div class="mb-3">
-				<input type="text" name="name" placeholder="Product Name"
-					   class="w-full px-2 py-1 border-solid border-2 border-slate-400 rounded-md hover:border-slate-900
-					   focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-300"
-				>
+				<input type="text" name="name" placeholder="Product Name" class="w-full custom-input">
 			</div>
 			
 			<div class="mb-3">
-				<select name="category"
-						class="w-full px-2 py-1 border-solid border-2 border-slate-400 rounded-md hover:border-slate-900
-						focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-300"
-				>
+				<select name="category" class="w-full custom-input">
 					<?php foreach ($categories as $category) : ?>
-						<option><?= $category['name']; ?></option>
+						<option value="<?= $category['id'] ?>"><?= $category['name']; ?></option>
 					<?php endforeach; ?>
 				</select>
 			</div>
 
 			<div class="mb-3">
-				<input type="text" name="name" placeholder="Description"
-					   class="w-full px-2 py-1 border-solid border-2 border-slate-400 rounded-md hover:border-slate-900
-					   focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-300"
-				>
+				<input type="text" name="description" placeholder="Description" class="w-full custom-input">
 			</div>
 			
 			<div class="flex flex-row items-center justify-end mt-4">
-				<button type="submit"
-						class="text-slate-200 bg-slate-800 px-4 py-2 rounded-md shadow-md hover:text-slate-100
-						hover:bg-slate-900 focus:ring-2 focus:ring-offset-2 focus:ring-slate-300"
-				>
-					Add product
-				</button>
+				<button type="submit"class="custom-primary-button">Add product</button>
 			</div>
 		</div>
 	</form>
--- views/products/edit.php
+++ views/products/edit.php
@@ -1,12 +1,37 @@
-<h1>Edit Product - <?= $product['name']; ?> </h1>
+<div class="w-2/5 my-4">
+	<h1 class="text-2xl mb-10">Edit Product - <?= $product['name']; ?> </h1>
 
-<div><a href="/index.php?p=products&action=list">Back to list</a></div>
+	<div class="mb-10">
+		<a href="/index.php?p=products&action=list" class="custom-secondary-button">
+			&larr; Back to list
+		</a>
+	</div>
 
-<form method="POST">
-	<div><?= $error ?? ''; ?></div>
+	<form method="POST">
+		<?php include_once ROOT_DIR . '/views/includes/alert-error.php'; ?>
+		<?php include_once ROOT_DIR . '/views/includes/alert-success.php'; ?>
 
-	<input type="text" name="name" value="<?= $product['name'] ?>">
-	<button type="submit">Save</button>
-</form>
+		<div class="mb-3">
+			<input type="text" name="name" value="<?= $product['name']; ?>" class="w-full custom-input">
+		</div>
 
+		<div class="mb-3">
+			<select name="category" class="w-full custom-input">
+				<?php foreach ($categories as $category) : ?>
+					<option value="<?= $category['id']; ?>" <?= $category['id'] === $product['category_id'] ? 'selected': ''; ?>>
+						<?= $category['name']; ?>
+					</option>
+				<?php endforeach; ?>
+			</select>
+		</div>
+
+		<div class="mb-3">
+			<input type="text" name="description" placeholder="Description" value="<?= $product['description']; ?>" class="w-full custom-input">
+		</div>
+
+		<div class="flex flex-row items-center justify-end">
+			<button type="submit" class="custom-primary-button">Save</button>
+		</div>
+	</form>
+</div>
 
