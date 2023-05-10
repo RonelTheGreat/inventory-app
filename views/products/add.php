78b82b7b073ed157978e5e605cbeda8fed51b494
Ronel Carlo Berino <ronel.carlo@pinnaclecart.com>
2023-05-10 17:36:16 +0800

    Updated add-product view

--- views/products/add.php
+++ views/products/add.php
@@ -11,31 +11,33 @@
 		<?php include_once ROOT_DIR . '/views/includes/alert-error.php'; ?>
 		<?php include_once ROOT_DIR . '/views/includes/alert-success.php'; ?>
 
-		<div>
-			<div class="mb-3">
-				<input type="text" name="name" placeholder="Product Name" value="<?= $request['old']['name'] ?? ''; ?>" class="w-full custom-input">
-			</div>
-			
-			<div class="mb-3">
-				<?php $selectedId = $request['old']['category'] ?? 0; ?>
-				<select name="category" class="w-full custom-input">
-					<?php foreach ($categoryOptions as $id => $name) : ?>
-						<option value="<?= $id ?>" <?= $selectedId == $id ? 'selected' : ''; ?>><?= $name; ?></option>
-					<?php endforeach; ?>
-				</select>
-			</div>
+		<div class="mb-4">
+			<label class="custom-input-label">Name</label>
+			<input type="text" name="name" value="<?= $request['old']['name'] ?? ''; ?>" class="w-full custom-input">
+		</div>
 
-			<div class="mb-3">
-				<input type="text" name="description" placeholder="Description" value="<?= $request['old']['description'] ?? ''; ?>" class="w-full custom-input">
-			</div>
+		<div class="mb-3">
+			<label class="custom-input-label">Category</label>
+			<?php $selectedId = $request['old']['category'] ?? 0; ?>
+			<select name="category" class="w-full custom-input">
+				<?php foreach ($categoryOptions as $id => $name) : ?>
+					<option value="<?= $id ?>" <?= $selectedId == $id ? 'selected' : ''; ?>><?= $name; ?></option>
+				<?php endforeach; ?>
+			</select>
+		</div>
 
-			<div class="mb-3">
-				<input type="number" name="price" placeholder="Price" step="0.01" value="<?= $request['old']['price'] ?? ''; ?>" class="w-full custom-input">
-			</div>
-			
-			<div class="flex flex-row items-center justify-end mt-4">
-				<button type="submit"class="custom-primary-button">Add product</button>
-			</div>
+		<div class="mb-4">
+			<label class="custom-input-label">Description</label>
+			<textarea name="description" rows="3" class="w-full custom-input"><?= $request['old']['description'] ?? ''; ?></textarea>
+		</div>
+
+		<div class="mb-4">
+			<label class="custom-input-label">Price</label>
+			<input type="number" name="price" step="0.01" value="<?= $request['old']['price'] ?? ''; ?>" class="w-full custom-input">
+		</div>
+
+		<div class="flex flex-row items-center justify-end mt-4">
+			<button type="submit"class="custom-primary-button">Add product</button>
 		</div>
 	</form>
 </div>
