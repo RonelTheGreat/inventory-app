5f1f736785f6024a49778111c3e109de3d2f1638
Ronel Carlo Berino <ronel.carlo@pinnaclecart.com>
2023-06-20 20:40:31 +0800

    Preliminary sidebar and admin layout

---
+++ views/includes/sidebar.php
@@ -0,0 +1,22 @@
+<div class="w-64 bg-slate-800 text-slate-200 hover:text-slate-100 pt-8">
+	<div class="px-4 py-3 hover:bg-slate-900/10">
+		<a href="#" class="w-100 block">
+			<i class="far fa-chart-bar"></i>
+			<span class="ml-1">Dashboard</span>
+		</a>
+	</div>
+
+	<div class="px-4 py-3 hover:bg-slate-900/10">
+		<a href="/index.php?p=products&action=list" class="w-100 block">
+			<i class="fas fa-box-open"></i>
+			<span class="ml-1">Products</span>
+		</a>
+	</div>
+
+	<div class="px-4 py-3 hover:bg-slate-900/10">
+		<a href="/index.php?p=categories&action=list" class="w-100 block">
+			<i class="fas fa-boxes"></i>
+			<span class="ml-1">Categories</span>
+		</a>
+	</div>
+</div>
