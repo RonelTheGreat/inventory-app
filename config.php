894045dad27b4d01afbdd33a4f4291b23532eb2b
Ronel Carlo Berino <ronel.carlo@pinnaclecart.com>
2023-06-22 20:41:43 +0800

    Added tab

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
 
-define("ROOT_DIR", dirname(__FILE__));
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'inventory_app');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('HTTPS_URL', 'http://localhost');
+	define("ROOT_DIR", dirname(__FILE__));
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', 'inventory_app');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', '');
+	define('HTTPS_URL', 'http://localhost');
+
--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php
 
-require_once './config.php';
+	require_once './config.php';
 
-require_once ROOT_DIR . '/models/Database.php';
-require_once ROOT_DIR . '/controllers/BaseController.php';
-require_once ROOT_DIR . '/controllers/Router.php';
-require_once ROOT_DIR . '/core/Request.php';
+	require_once ROOT_DIR . '/models/Database.php';
+	require_once ROOT_DIR . '/controllers/BaseController.php';
+	require_once ROOT_DIR . '/controllers/Router.php';
+	require_once ROOT_DIR . '/core/Request.php';
 
-$router = new Router();
-$router->setRoutes(ROOT_DIR . '/routes.php');
-$controllerFile = $router->getRoute( $_REQUEST['p'] ?? '', $_REQUEST['action'] ?? '');
+	$router = new Router();
+	$router->setRoutes(ROOT_DIR . '/routes.php');
+	$controllerFile = $router->getRoute( $_REQUEST['p'] ?? '', $_REQUEST['action'] ?? '');
 
-if ($controllerFile === false) exit('Page not found.');
+	if ($controllerFile === false) exit('Page not found.');
 
-require_once  $controllerFile;
+	require_once  $controllerFile;
 
-$className = ucwords($router->getPage());
-$controllerInstance = new $className(new Database(), new Request());
+	$className = ucwords($router->getPage());
+	$controllerInstance = new $className(new Database(), new Request());
 
-$controllerInstance->setViewLayout($controllerInstance instanceof Login ? 'login' : 'default');
-$controllerInstance->setViewDirectoryName($router->getPage());
-$handler = $controllerInstance->getHandler($router->getAction(), $_SERVER['REQUEST_METHOD']);
-call_user_func([$controllerInstance, $handler]);
+	$controllerInstance->setViewLayout($controllerInstance instanceof Login ? 'login' : 'default');
+	$controllerInstance->setViewDirectoryName($router->getPage());
+	$handler = $controllerInstance->getHandler($router->getAction(), $_SERVER['REQUEST_METHOD']);
+	call_user_func([$controllerInstance, $handler]);
 
