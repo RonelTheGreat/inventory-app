d45ee21c27b88d8f6760f214f0d6e0c1a3c524d3
Ronel Carlo Berino <ronel.carlo@pinnaclecart.com>
2023-08-24 21:43:24 +0800

    Removed config

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	define("ROOT_DIR", dirname(__FILE__));
-	define('DB_HOST', 'localhost');
-	define('DB_NAME', 'inventory_app');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
-	define('HTTPS_URL', 'http://localhost');
-	define('ADMIN_SESSION_TIMEOUT_IN_SECONDS', 3600);
