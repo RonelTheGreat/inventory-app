174b356268e0f9018c0c027baab3162ea7e10b8e
Ronel Carlo Berino <ronel.carlo@pinnaclecart.com>
2023-08-30 19:45:34 +0800

    Updated Session core class; added getter and setter function

--- app/classes/core/Session.php
+++ app/classes/core/Session.php
@@ -7,4 +7,12 @@ class Session {
 	public function __construct() {
 		session_start();
 	}
+
+	public function set(string $name, mixed $value) {
+		$_SESSION[$name] = $value;
+	}
+
+	public function get(string $name, mixed $default = null): mixed {
+		return $_SESSION[$name] ?? $default;
+	}
 }
